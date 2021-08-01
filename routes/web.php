5088eaea2b237f537f39a8d7ecede3ca262f5ac0
Onur MUTLU <onurmutlu@gmail.com>
2021-08-01 18:26:17 +0300

    link

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', function () { return view('home'); });
+Route::get('/about', function () { return view('about'); });
+Route::get('/blog', function () { return view('blog'); });
+Route::get('/about', function () { return view('about'); });
