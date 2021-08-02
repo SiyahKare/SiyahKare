5c5d0ec1f012e6a2b015a7d22be24119816614d2
huseyincetinkoz <hcetinkozz@icloud.com>
2021-08-02 11:15:56 +0300

    s

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -90,7 +90,7 @@
         <div class="row mb-5">
           <div class="col-md-6 col-lg-3 d-flex align-self-stretch ftco-animate">
             <div class="media block-6 services d-block text-center">
-              <div class="d-flex justify-content-center"><div class="icon color-3 d-flex justify-content-center mb-3"><span class="align-self-center icon-lightbulb-o"></span></div></div>
+            <img width="100px" src="images/number1.png" alt="">
               <div class="media-body p-2 mt-3">
                 <h3 class="heading">Strategy</h3>
                 <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
@@ -99,7 +99,7 @@
           </div>
           <div class="col-md-6 col-lg-3 d-flex align-self-stretch ftco-animate">
             <div class="media block-6 services d-block text-center">
-              <div class="d-flex justify-content-center"><div class="icon color-1 d-flex justify-content-center mb-3"><span class="align-self-center icon-laptop"></span></div></div>
+            <img width="100px" src="images/number2.png" alt="">
               <div class="media-body p-2 mt-3">
                 <h3 class="heading">Design</h3>
                 <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
