66ed699d8f6f83f53fdc9c99eff9c123e57f5577
huseyincetinkoz <hcetinkozz@icloud.com>
2021-08-02 10:55:43 +0300

    videoyu değiştirdim

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -83,7 +83,7 @@
 
         <div class="card-header card-header-image">
                             <div class="embed-responsive embed-responsive-16by9">
-                                <iframe src="https://player.vimeo.com/video/558569397?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="ERC-İzmitPlus"></iframe><script src="https://player.vimeo.com/api/player.js"></script>
+                            <iframe src="https://player.vimeo.com/video/558369015?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="ERC-Merkez"></iframe><script src="https://player.vimeo.com/api/player.js"></script>
                             </div>
 
                             
