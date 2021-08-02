f81126e29cbacdfaff3c7a0596406851d983d0fb
huseyincetinkoz <hcetinkozz@icloud.com>
2021-08-02 11:47:43 +0300

    servislerimiz

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,9 @@ Route::get('/about', function () { return view('about'); });
 Route::get('/home', function () { return view('home'); });
 Route::get('/contact', function () { return view('contact'); });
 Route::get('/blog', function () { return view('blog'); });
+// servislerimi
 Route::get('/services', function () { return view('services'); });
+Route::get('/services2', function () { return view('services2'); });
 Route::get('/portfolio', function () { return view('portfolio'); });
 
 
