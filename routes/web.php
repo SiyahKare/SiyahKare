0989b578a4abe35e4457a0358a2c7a6e2bd7dd5e
huseyincetinkoz <hcetinkozz@icloud.com>
2021-08-02 09:42:00 +0300

    contact

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () { return view('home'); });
 Route::get('/about', function () { return view('about'); });
 Route::get('/home', function () { return view('home'); });
+Route::get('/contact', function () { return view('contact'); });
 Route::get('/blog', function () { return view('blog'); });
 Route::get('/about', function () { return view('about'); });
