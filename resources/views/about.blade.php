e4e7faccdbbdfd064c4ffa9f204887b4873807ae
huseyincetinkoz <hcetinkozz@icloud.com>
2021-08-02 17:38:20 +0300

    misyonend

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -72,18 +72,7 @@
     </div>
 </div>
 
-<div class="bg-light">
-      <section class="ftco-section-featured ftco-section-featured-2 ftco-animate">
-        <div class="container-fluid" data-scrollax-parent="true">
-          <div class="row mt-5 d-flex justify-content-center">
-            <div class="col-md-8 text-center heading-section ftco-animate">
-            <h2 class="h1"> <strong style="color: black;">SiyahKare Dijital Çözümler</strong></h2>
-              <p>Ekibimiz 1995 yılından bu yana bilişim sektöründe etkin çalışan, <br> yüzden fazla dijital projeye imza atmış <br> her biri alanında uzman kişilerin SiyahKare markası altında toplanmasıyla oluşmuştur.</p>
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
+
 
     <div class="ftco-section">
       <div class="container">
