5dc367cad014935bd8ac475d6000da59493e5185
huseyincetinkoz <hcetinkozz@icloud.com>
2021-08-10 21:04:24 +0300

    vimeo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -178,8 +178,13 @@
                     <div class="card-header card-header-image">
                             <div class="embed-responsive embed-responsive-16by9">
                             <iframe src="https://player.vimeo.com/video/558369015?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="ERC-Merkez"></iframe><script src="https://player.vimeo.com/api/player.js"></script>
+
+                                <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/500399158?title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+                                <p><a href="https://vimeo.com/500399158">Who We Are</a> from <a href="https://vimeo.com/siyahkare">siyahkare</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
                             </div>
                             
+
+
                             
                 </div>
             </div>
